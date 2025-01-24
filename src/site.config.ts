621f0e20d2f472b731c3132e5b7f8b05007a17fc
Mathilde Arconte <signup@marconte.fr>
2025-01-24 22:10:32 +0100

    Refactor la structure des composants de blog pour améliorer la lisibilité et la gestion des données

--- src/site.config.ts
+++ src/site.config.ts
@@ -9,22 +9,21 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'Andrés Bedoya', // Site author
-	title: 'Astro Kubrick Blog', // Site title.
+	author: 'Thatmuch', // Site author
+	title: 'Le blog de l\'équipage', // Site title.
 	description: 'The default Astro theme based on the famous Kubrick from WordPress.', // Description to display in the meta tags
-	lang: 'en-GB',
-	ogLocale: 'en_GB',
-	shareMessage: 'Share this post', // Message to share a post on social media
+	lang: 'fr-FR',
+	ogLocale: 'fr-FR',
+	shareMessage: 'Partager', // Message to share a post on social media
 	paginationSize: 4 // Number of posts per page
 }
 
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'Développement',
+	'Design',
+	'Projet',
+	'Communication',
 ] as const
 
 export const BLOGROLL = [
