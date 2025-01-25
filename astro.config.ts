9efe1e031521592fdbe58556840257266bbed151
Mathilde Arconte <signup@marconte.fr>
2025-01-25 14:37:01 +0100

    Mise à jour de la configuration TypeScript et ajout de dépendances React dans package.json, intégration de React dans la configuration Astro

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://the-kubrick-astro.vercel.app/",
   integrations: [tailwind(), sitemap(), mdx({
     syntaxHighlight: "shiki"
-  })],
+  }), react()],
   markdown: {
     shikiConfig: {
       theme: "one-dark-pro",
@@ -17,6 +18,6 @@ export default defineConfig({
     }
   },
   image: {
-    service: passthroughImageService(),
-  },
+    service: passthroughImageService()
+  }
 });
